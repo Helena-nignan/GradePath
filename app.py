594ba5fbd98312463f0d4b336348cb5f59e874ac
nignanhulda <nignanhulda@gmail.com>
2025-08-27 09:22:18 +0000

    scikit-learn 1.7.1

--- app.py
+++ app.py
@@ -383,7 +383,7 @@ if page == "Predict Performance":
             input_df = pd.DataFrame([input_dict])
             
             try:
-                model = joblib.load('student.pkl')
+                model = joblib.load('model.joblib')
                 prediction = model.predict(input_df)[0]
                 
                 # Store results in session state
